9dc74e7821547666c32feaea66494ce78897bca5
DaeHyunKang <dhkang@sudo.co.kr>
2019-06-03 23:50:57 +0900

    PreView FileList 추가

--- FileNameChanger/ViewModels/MainWindowViewModel.cs
+++ FileNameChanger/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace FileNameChanger.ViewModels
 {
@@ -10,7 +11,37 @@ namespace FileNameChanger.ViewModels
         public string DirectoryPath
         {
             get { return directoryPath; }
-            set { directoryPath = value; }
+            set
+            {
+                directoryPath = value;
+                OnPropertyChanged("DirectoryPath");
+            }
         }
+
+        private List<PreviewFileFormat> previewFileList = new List<PreviewFileFormat>();
+        public List<PreviewFileFormat> PreviewFileList
+        {
+            get { return previewFileList; }
+            set
+            {
+                previewFileList = value;
+                OnPropertyChanged("PreviewFileList");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+    public class PreviewFileFormat
+    {
+        public string OriginalFileName { get; set; }
+        public string ChangedFileName { get; set; }
     }
 }
