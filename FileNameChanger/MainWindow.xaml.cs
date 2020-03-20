0d43eee42ffb78d25f502bdfd966bd4574014e60
Hyolo <soejdtodxn@gmail.com>
2020-03-20 19:09:57 +0900

    implement change logic
    변경할 텍스트 입력할 컨트롤 추가 및
    기본 동작 하도록 로직만 수정

--- FileNameChanger/MainWindow.xaml.cs
+++ FileNameChanger/MainWindow.xaml.cs
@@ -30,9 +30,28 @@ namespace FileNameChanger
 
                     foreach(var file in fileList)
                     {
-                        //TODO: 수식 받아와서 변경하도록
                         viewModel.PreviewFileList.Add(new PreviewFileFormat() { OriginalFileName = file, ChangedFileName = file + "ex"});
                     }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var viewModel = DataContext as MainWindowViewModel;
+
+            foreach (var file in viewModel.PreviewFileList)
+            {
+
+                //1. file명에 Before가 포함되어있으면
+                //2. 해당 파일명의 그 부분을 after로 변경
+
+                if (file.OriginalFileName.Contains(Before.Text))
+                {
+                    var oldFileName = file.OriginalFileName;
+                    var newFileName = file.OriginalFileName.Replace(Before.Text, After.Text);
+                    // 실제로 파일 명 변경
+                    File.Move(oldFileName, newFileName);
                 }
             }
         }
